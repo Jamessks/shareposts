d492d402a066eb075b961a829c915bd57425b5d5
Jamessks <59316989+Jamessks@users.noreply.github.com>
2020-05-20 17:17:42 +0300

    Add files via upload

---
+++ app/config/mailconfig.php
@@ -0,0 +1,12 @@
+<?php
+
+return ['mail' =>
+	[
+	'MAIL_HOST'=> '', // smtp host
+	'MAIL_FROM_ADDRESS'=>'',// myemail@example.com
+	'MAIL_PASSWORD'=>'',// email's password
+	'MAIL_USER'=> '',// myemail@example.com
+	'MAIL_PORT'=> '',
+	'MAIL_ENCTYPE' => ''
+	]
+];
